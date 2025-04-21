4ac78b282d29e1d16b78c1a7e1cf9a0b0c51e1d7
sj <sj@sweettracker.co.kr>
2025-04-21 16:04:28 +0900

    chore: dummy data 날짜 수정

--- lib/features/plan/data/dummy_plan_data.dart
+++ lib/features/plan/data/dummy_plan_data.dart
@@ -3,6 +3,17 @@ import 'package:earth_trip/features/plan/domain/entities/plan_entity.dart';
 class DummyPlanRepository {
   static List<PlanEntity> fetchUpcomingPlans() => [
     PlanEntity(
+      title: '전주 여행',
+      start: DateTime(2025, 5, 1),
+      end: DateTime(2025, 5, 1),
+      cost: 596800,
+      daysLeft: DateTime(2025, 5, 1).difference(DateTime.now()).inDays,
+      flightDuration: '00시간 00분',
+      stayDuration: '00시간 00분',
+      likes: 0,
+      participants: 2,
+    ),
+    PlanEntity(
       title: '스페인 여행',
       start: DateTime(2025, 9, 26),
       end: DateTime(2025, 10, 13),
@@ -14,22 +25,11 @@ class DummyPlanRepository {
       participants: 2,
     ),
     PlanEntity(
-      title: '전주 여행',
-      start: DateTime(2025, 9, 26),
-      end: DateTime(2025, 10, 13),
-      cost: 596800,
-      daysLeft: DateTime(2025, 9, 26).difference(DateTime.now()).inDays,
-      flightDuration: '00시간 00분',
-      stayDuration: '00시간 00분',
-      likes: 0,
-      participants: 2,
-    ),
-    PlanEntity(
       title: '발리 여행',
-      start: DateTime(2025, 9, 26),
-      end: DateTime(2025, 10, 13),
+      start: DateTime(2026, 5, 26),
+      end: DateTime(2026, 5, 31),
       cost: 3230000,
-      daysLeft: DateTime(2025, 9, 26).difference(DateTime.now()).inDays,
+      daysLeft: DateTime(2026, 5, 26).difference(DateTime.now()).inDays,
       flightDuration: '7시간 15분',
       stayDuration: '8시간 20분',
       likes: 3,
